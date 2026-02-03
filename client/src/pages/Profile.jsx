b48140ebf5561cae6d25d3f157eef550eceeb22e
anjali-gupta8 <anjaligupta8102001@gmail.com>
2026-02-03 19:14:50 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signOutUserStart, signOutUserSuccess, signOutUserFailure } from '../redux/user/userSlice'
 import { useDispatch } from "react-redux";
+import { Link } from 'react-router-dom'
 
 export default function Profile() {
   const { currentUser, loading, error }=useSelector((state)=>state.user);
@@ -132,6 +133,9 @@ export default function Profile() {
         <input type="email" placeholder="email" defaultValue={currentUser.email} id="email" className="border p-3 rounded-lg" onChange={handleChange} />
         <input type="password" placeholder="password" id="password" className="border p-3 rounded-lg" onChange={handleChange} />
         <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading? 'loading...' : 'update'}</button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
